cfe4263f4bc3e2424e7f9727ada9f7120811ac1a
dkoomson05-gif <dkoomson05@gmail.com>
2026-05-23 12:43:38 -0600

    Update HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -26,7 +26,7 @@ const MAX_IMAGE_SIZE = 1000000
 /* ENTER YOUR ENDPOINT HERE
    FILES UPLOADED TO MY ENDPOINT ARE AUTOMATICALLY DELETED EVERY FEW HOURS */
 
-const API_ENDPOINT = 'https://mk8e4v63el.execute-api.us-east-1.amazonaws.com/default/myfunction1'
+const API_ENDPOINT = 'https://mlmslc35o9.execute-api.us-east-1.amazonaws.com/default/myfunction1'
 
 export default {
   name: 'app',
